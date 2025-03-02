e7bfecca7668213dd7bb1c3cd797e26b4095df00
João Lopes <joaopclopes@gmail.com>
2025-03-03 00:33:16 +0100

    move footer to the bottom

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,19 @@ function App() {
   return (
     <>
       <h1>Sort Them All!</h1>
-      {gameStarted && <Game /> || (
-        <div style={{flexGrow: 1}}>
-          <p>
-            Drag the items of the list to sort them in <strong>ascending</strong> order.
-          </p>
-          <p>
-            A new challenge is created each day.
-          </p>
-          <button onClick={startGame}>Play</button>
-        </div>
-      )}
+      <div style={{flexGrow: 1}}>
+        {gameStarted && <Game /> || (
+          <div>
+            <p>
+              Drag the items of the list to sort them in <strong>ascending</strong> order.
+            </p>
+            <p>
+              A new challenge is created each day.
+            </p>
+            <button onClick={startGame}>Play</button>
+          </div>
+        )}
+      </div>
       <footer>
         by <a href="https://github.com/lopis/sort-them-all">@lopis</a>
       </footer>
