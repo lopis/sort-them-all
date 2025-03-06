e874b0a88663d48015518e2e15bf6e1e1ef4997c
João Lopes <joaopclopes@gmail.com>
2025-03-06 21:51:26 +0100

    Add share button

--- src/game/Modal.jsx
+++ src/game/Modal.jsx
@@ -1,6 +1,7 @@
 import ScoreTable from './ScoreTable';
 import './Modal.css';
 import { useState } from 'react';
+import { OPTION_COUNT } from '../api/ApiDataProvider';
 
 const Modal = ({ scores, gameNumber, onClose }) => {
   const [closing, setClosing] = useState(false);
@@ -10,6 +11,29 @@ const Modal = ({ scores, gameNumber, onClose }) => {
     setTimeout(() => {
       onClose()
     }, 100);
+  }
+
+  const share = () => {
+    const shareText = [
+      `Sort-Them-All #${gameNumber}`,
+      window.location.hostname,
+      `🌱💦🔥 | Guesses: ${scores.length}`,
+      ...Array.from({ length: OPTION_COUNT }).map((_, colIndex) => (
+        scores.map((scoreRow, rowIndex) => (
+          <span key={rowIndex}>{scoreRow[colIndex] ? '🔴' : '⚫️'}</span>
+        ))
+      ))
+    ]
+
+    if (navigator.share) {
+      navigator.share({
+        title: `Sort-Them-All #${gameNumber}`,
+        text: shareText.join('\n'),
+        url: window.location.href,
+      }).catch((error) => console.error('Error sharing', error));
+    } else {
+      alert('Web Share API is not supported in your browser.');
+    }
   }
 
   return <div class={`modal ${closing ? 'closing' : ''}`}>
@@ -28,7 +52,7 @@ const Modal = ({ scores, gameNumber, onClose }) => {
       </p>
       <ScoreTable scores={scores} />
       <div class="buttonGroup">
-        <button>Share</button>
+        <button onClick={share}>Share</button>
         <button onClick={close}>Close</button>
       </div>
     </div>
