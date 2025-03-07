22bc2efb684e5924ac68b94a03ddde90960a0da2
João Lopes <joaopclopes@gmail.com>
2025-03-07 22:56:32 +0100

    Add generation practice

--- src/api/ApiDataProvider.jsx
+++ src/api/ApiDataProvider.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Pokedex } from 'pokeapi-js-wrapper';
 import Prando from 'prando';
 import ApiDataContext from './ApiDataContext';
@@ -36,17 +36,20 @@ const shuffleArray = (array) => {
   return array;
 };
 
-const ApiDataProvider = ({ practice, children }) => {
+const ApiDataProvider = ({ practice, generation, children }) => {
   const [pokemonList, setPokemonList] = useState([]);
   const [correctOrder, setCorrectOrder] = useState([]);
   const [sortingCriteria, setSortingCriteria] = useState('');
   const [loading, setLoading] = useState(true);
 
-  const fetchPokemon = async (rng) => {
-    rng.reset();
-    const list = await Promise.all(
+  const fetchFromAllPokemon = async (rng) => {
+    const generatedIds = new Set();
+    return Promise.all(
       Array.from({ length: OPTION_COUNT }, async () => {
-        const pokemonId = rng.next(0, TOTAL_POKEMON_COUNT - 1);
+        let pokemonId;
+        do {
+          pokemonId = rng.next(0, TOTAL_POKEMON_COUNT - 1);
+        } while (generatedIds.has(pokemonId));
         const result = await P.getPokemonsList({ offset: pokemonId, limit: 1 });
         const pokemon = result.results[0];
         const pokemonData = await P.getPokemonByName(pokemon.name);
@@ -60,13 +63,51 @@ const ApiDataProvider = ({ practice, children }) => {
         return pokemonData;
       })
     );
+  };
+
+  const fetchFromGeneration = async (rng, gen) => {
+    const generation = await P.getGenerationByName(gen);
+    
+    const species = generation.pokemon_species;
+    const pokemonCount = species.length;
+    const generatedIds = new Set();
+    return Promise.all(
+      Array.from({ length: OPTION_COUNT }, async () => {
+        let pokemonId;
+        do {
+          pokemonId = rng.nextInt(0, pokemonCount - 1);
+        } while (generatedIds.has(pokemonId));
+        generatedIds.add(pokemonId);
+        const pokemon = await P.getPokemonByName(species[pokemonId].name);
+        const pokemonData = await P.getPokemonByName(pokemon.name);
+        pokemonData.stats.forEach(({ base_stat, stat }) => {
+          pokemonData[stat.name] = base_stat;
+        });
+        pokemonData.isGmax = pokemon.name.endsWith('gmax');
+        if (pokemon.name.includes('-') && !hyphenatedPokemonNames.some(name => pokemon.name.includes(name))) {
+          pokemonData.label = pokemon.name.split('-')[1]; // TODO: is there always just one hyphen on can there be more?
+        }
+        return pokemonData;
+      })
+    );
+  };
+
+  const getPokemonList = useCallback(async (rng, gen) => {
+    rng.reset();
+    let list;
+    if (gen) {
+      list = await fetchFromGeneration(rng, gen);
+    } else {
+      list = await fetchFromAllPokemon(rng);
+    }
+    
     rng.reset();
     const criterion = rng.nextArrayItem(criteriaList);
     setPokemonList(shuffleArray(list));
     setSortingCriteria(criterion);
     setCorrectOrder([...list].sort((a, b) => a[criterion] - b[criterion]).map((pokemon) => pokemon[criterion]));
     setLoading(false);
-  };
+  }, []);
 
   useEffect(() => {
     setLoading(true);
@@ -90,8 +131,8 @@ const ApiDataProvider = ({ practice, children }) => {
       rng = new Prando(seed);
     }
 
-    fetchPokemon(rng);
-  }, [practice]);
+    getPokemonList(rng, generation);
+  }, [practice, generation, getPokemonList]);
 
   return (
     <ApiDataContext.Provider value={{ pokemonList, sortingCriteria, correctOrder, loading }}>
