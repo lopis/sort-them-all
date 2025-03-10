efdac6bf6c20b3d9487b37b6de6ccfac14284baa
João Lopes <joaopclopes@gmail.com>
2025-03-10 21:25:20 +0100

    setup server and network

---
+++ server/docker-setup-network.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the custom network and the third-party image name
+NETWORK_NAME="pokeapi_network"
+THIRD_PARTY_IMAGE="pokeapi-app"
+
+# Get the container ID of the third-party container
+CONTAINER_ID=$(docker ps --filter "name=$THIRD_PARTY_IMAGE" --format "{{.ID}}")
+
+echo $CONTAINER_ID
+
+# Check if the container ID is found
+if [ -z "$CONTAINER_ID" ]; then
+  echo "Third-party container not found."
+  exit 1
+fi
+
+# Connect the third-party container to the custom network
+docker network connect $NETWORK_NAME $CONTAINER_ID
+
+echo "Connected third-party container ($CONTAINER_ID) to network $NETWORK_NAME."
